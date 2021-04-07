6978b4c5d18b6d329a36027b1e7156b4a4c8530f
LuizzP <luizz.paullo@gmail.com>
2021-04-07 00:45:41 -0400

    fim do curso

--- src/server.js
+++ src/server.js
@@ -9,8 +9,45 @@ app.set("view engine", "ejs");
 
 // Criando as Rotas.
 // Rota da Pagina Index
+
 app.get("/", (req, res) => {
-    res.render("pages/index"); //Nao precisa pasta /views pq ja eh o default do ejs
+
+    const items = [
+        {
+            title: "D",
+            message: "eveloperment app fast"
+        },
+        {
+            title: "E",
+            message: "ffective, Elegant e Easy debugging"
+        },
+        {
+            title: "M",
+            message: "arkup html plain JS"
+        },
+        {
+            title: "A",
+            message: "ctive development"
+        },
+        {
+            title: "I",
+            message: "nstall EJS via NPM"
+        },
+        {
+            title: "S",
+            message: "peed Execution e Simple syntax"
+        },
+
+    ];
+
+    const subtitle = "Uma linguagem de modelagem para criação de paginas HTML com JS"
+
+
+     //Nao precisa pasta /views pq ja eh o default do ejs
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle,
+    });
 })
 
 // Rota da Pagina sobre
